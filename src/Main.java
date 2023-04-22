222e66c4cbc30bb0f6b55c5a6da1c974a95cafec
Eugene Bukhanov <buhanza21@yandex.ru>
2023-04-22 21:02:08 +0300

    fix: тестовые позиции

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,26 @@
+import model.Task;
 import service.Manager;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Manager manager = new Manager();
-        Scanner scanner = new Scanner(System.in);
-        String name;
-        String description;
-        int exit = 0;
-        int in;//Переименовать
-        int id;
-        do {
-            menu();
-            in = scanner.nextInt();
-            switch (in) {
-                case 1:
-                    System.out.println("Имя");
-                    name = scanner.next();
 
-                    System.out.println("Описание");
-                    description = scanner.next();
 
-                    manager.makeTask(name,description);
-                    break;
-                case 2:
-                    System.out.println("Все задачи");
-                    manager.getAllTask();
-                    break;
-                case 3:
-                    System.out.println("Удалить все задачи");
-                    manager.deleteAllTask();
-                case 4:
-                    System.out.println("Введите id");
-                    id = scanner.nextInt();
-                    manager.removeById(id);
-            }
-            System.out.println();
-        } while (exit != in);
+        manager.makeTask(new Task("Задача - 1", "Тест - 1",
+                manager.identifier(), manager.NEW_PROGRESS));
+        manager.makeTask(new Task("Задача - 2", "Тест - 2",
+                manager.identifier(), manager.NEW_PROGRESS));
+
+        manager.getAllTask();
+
+
+        manager.updateTask(new Task("Задача - 1", "Тест - 3",
+                manager.identifier(), manager.NEW_PROGRESS));
+
+        manager.getAllTask();
+        manager.getById(1);
     }
     //Сменить тестовую реализацию !!!
-    public static void menu() {
-        System.out.println("1 - Создать задачу");
-        System.out.println("2 - Вывести все задачи");
-        System.out.println("3 - Удалить все задачи");
-        System.out.println("4 - Удалить задачу по id");
-    }
+
 }
